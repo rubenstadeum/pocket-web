49307f7d68873288cee6afe08f9a866dbe588a27
rubenstadeum <rubenstadeumoura@gmail.com>
2024-09-16 12:47:57 -0300

    Screen 1

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,22 @@
+import { Plus } from 'lucide-react'
+
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+    </div>
   )
 }
