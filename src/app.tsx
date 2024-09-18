b4e927ed41e14c2225ac4bdc0ec404edc1468318
rubenstadeum <rubenstadeumoura@gmail.com>
2024-09-18 16:56:43 -0300

    Separando arquivo get-summary

--- src/app.tsx
+++ src/app.tsx
@@ -5,29 +5,12 @@ import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
 import { useQuery } from '@tanstack/react-query'
-
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: string
-    }[]
-  >
-}
+import { getSummary } from './http/get-summary'
 
 export function App() {
-  const { data } = useQuery<SummaryResponse>({
+  const { data } = useQuery({
     queryKey: ['summary'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/summary')
-      const data = await response.json()
-
-      return data.summary
-    },
+    queryFn: getSummary
   })
 
   return (
